021853a0cdc48bad825cbe073c0b05089c5ceb90
Abdulrasaq Durosomo <durosomo.abdulrasaq@gmail.com>
2025-06-08 02:31:08 +0100

    add initial files

---
+++ src/main/kotlin/com/example/securecommerce/SecurecommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.securecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurecommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecurecommerceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/securecommerce/SecurecommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.securecommerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurecommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
